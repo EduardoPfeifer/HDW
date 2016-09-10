4d27e8b1b243fb48b75f99db57acdc2752992df9
brunocanella <brunocanella@yahoo.com.br>
2016-09-10 18:13:38 -0300

    Adicionando bibliotecas Timer0 e TX do EUSART

--- Testes.X/main.c
+++ Testes.X/main.c
@@ -11,7 +11,9 @@
 
 #include <pic16lf1786.h>
 
+#include "types.h"
 #include "timer.h"
+#include "usart.h"
 
 // Oscillator Selection (INTOSC oscillator: I/O function on CLKIN pin)
 #pragma config FOSC = INTOSC
@@ -28,19 +30,31 @@ void interrupt isr_general() {
     if( INTCONbits.TMR0IE == HIGH && INTCONbits.TMR0IF == HIGH ) {        
         timer0_isr();
     }
+    if( PIE1bits.TXIE == HIGH && PIR1bits.TXIF == HIGH ) {
+        usart_transmite_interrupt_isr();
+    }
 }
 
 void isr_timer0_pisca_led(void) {
+    // PORTBbits.RB4 = ~LATBbits.LATB4;
+}
+
+void isr_usart_tx_acende_led(void) {
     PORTBbits.RB4 = ~LATBbits.LATB4;
 }
 
 void main(void) {
     TRISBbits.TRISB4 = 0;
-    PORTBbits.RB4 = 0;    
+    PORTBbits.RB4 = 0;
     
     timer0_start( TIMER0_MODE_TIMER, PRESCALAR_USE_YES, PRESCALAR_RATE_256, isr_timer0_pisca_led );
     
+    usart_start( USART_SYNC_MODE_ASYNCHRONOUS, USART_BAUD_RATE_300 );
+    
     INTCONbits.GIE = HIGH;
+    
+    usart_transmite_lock_write_message( "Testezinho" );
+    isr_usart_tx_acende_led();
     
     while(1) {
         asm("nop");
